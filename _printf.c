35701e9de051287b50ba67664331449dbb420dbc
abela12 <abelberhanu6@gmail.com>
2022-04-19 12:04:19 -0600

    File - Updated

---
+++ _printf.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _printf - printf function
+ * @format: const char pointer
+ * Return: b_len
+ */
+int _printf(const char *format, ...)
+{
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
+
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
+}
--- main.h
+++ main.h
@@ -1,32 +1,52 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <string.h>
-#include <unistd.h>
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 
 /**
- * struct s_type- structure
- * @args- pointer arguments
- * @func: pointer function
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
  */
-typedef struct s_type
+typedef struct printHandler
 {
-	char *args;
-	int (*func)(va_list);
-} s_type;
-
-int _putchar(char c);
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+char *convert(unsigned long int num, int base, int lowercase);
 int _printf(const char *format, ...);
-int(*_typefor(const char *argu, int argb))(va_list);
-int p_char(va_list list);
-int p_string(va_list list);
-int p_int(va_list list);
-int count_num(unsigned int n);
-int p_rot13(char *s);
-int print_R(va_list list);
-int p_binary(va_list list);
-
+int (*get_print(char s))(va_list, flags_t *);
+int get_flag(char s, flags_t *f);
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+int _putchar(char c);
+int _puts(char *str);
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+int print_address(va_list l, flags_t *f);
+int print_percent(va_list l, flags_t *f);
 #endif
