c7c7b669f2d8bd7bb32de0ec92bbcd448b36e4c9
Nelson-Appiah-Agyei <126928090+Nelson-Appiah-Agyei@users.noreply.github.com>
2023-04-25 20:40:30 +0000

    Update _writechar.c

--- _writechar.c
+++ _writechar.c
@@ -11,20 +11,20 @@
  */
 int _putchar(char c)
 {
-	static char buf[1024];
-	static int i;
+    static char buf[1024];
+    static int i;
 
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buf, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buf[i] = c;
-		i++;
-	}
-	return (1);
+    if (c == -128 || i >= 1024)
+    {
+        write(1, buf, i);
+        i = 0;
+    }
+    if (c != -128)
+    {
+        buf[i] = c;
+        i++;
+    }
+    return (1);
 }
 
 /**
@@ -34,9 +34,9 @@ int _putchar(char c)
  */
 int _puts(char *str)
 {
-	register int i;
+    int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	return (i);
+    for (i = 0; str[i] != '\0'; i++)
+        _putchar(str[i]);
+    return (i);
 }
